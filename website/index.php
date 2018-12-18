5e6a2c8378f84a78581d9094815aa15015667943
mengmanzbh <1361006944@qq.com>
2018-12-18 15:17:01 +0800

    OK
    OK

---
+++ website/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my shop</title>
+</head>
+<body>
+	<h1>Welcome to my shop</h1>
+	<ul>
+		<?php
+		 $json = file_get_contents('http://product-service');
+		 $obj = json_decode($json);
+		 $products = $obj->products;
+		 foreach ($products as $product) {
+		 	echo "<li>$product</li>";
+		 }
+		?>
+	</ul>
+</body>
+</html>
